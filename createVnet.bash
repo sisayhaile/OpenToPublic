8cf5357937701365543caf02e2d3763fc5e08881
sisayhaile <89225090+sisayhaile@users.noreply.github.com>
2025-08-28 16:59:39 +0000

    version1

---
+++ createVnet.bash
@@ -0,0 +1,73 @@
+```
+#ResourceGroup
+az group create --name rg-demo --location westus3 --tags owner=Sisay environment=dev project=tbd resource=rg
+#Vnet
+    #Step 1: Create Hub vnet  with 5 Subnet
+        az network vnet create \
+          --name hubVnet \
+          --resource-group rg-demo \
+          --location westus3 \
+          --address-prefix 100.100.0.0/22 \
+          --tags owner=Sisay environment=dev project=tbd resource=vnet topolgy=hub
+        az network vnet subnet create --resource-group rg-demo --vnet-name hubVnet --name hubSubnet1 --address-prefix 100.100.0.0/24
+        az network vnet subnet create --resource-group rg-demo --vnet-name hubVnet --name hubSubnet2 --address-prefix 100.100.1.0/24
+        az network vnet subnet create --resource-group rg-demo --vnet-name hubVnet --name hubSubnet3 --address-prefix 100.100.2.0/24
+        az network vnet subnet create --resource-group rg-demo --vnet-name hubVnet --name hubSubnet4 --address-prefix 100.100.3.0/24
+
+    Step 2: Create spoke vnet with 3 Subnet
+        az network vnet create \
+          --name spokeVnetRed \
+          --resource-group rg-demo \
+          --location westus3 \
+          --address-prefix 100.101.0.0/22 \
+          --tags owner=Sisay environment=dev project=sample resource=vnet topolgy=spoke
+        az network vnet subnet create --resource-group rg-demo --vnet-name spokeVnetRed --name spokeRedSubnet1 --address-prefix 100.101.0.0/24
+        az network vnet subnet create --resource-group rg-demo --vnet-name spokeVnetRed --name spokeRedSubnet2 --address-prefix 100.101.1.0/24
+        az network vnet subnet create --resource-group rg-demo --vnet-name spokeVnetRed --name spokeRedSubnet3 --address-prefix 100.101.2.0/24
+        az network vnet subnet create --resource-group rg-demo --vnet-name spokeVnetRed --name spokeRedSubnet4 --address-prefix 100.101.3.0/24
+
+    #Step 3: Create spoke vnet with 3 Subnet
+        az network vnet create \
+          --name spokeVnetBlue \
+          --resource-group rg-demo \
+          --location westus3 \
+          --address-prefix 100.102.0.0/22 \
+          --tags owner=Sisay environment=dev project=sample resource=vnet topolgy=spoke
+        az network vnet subnet create --resource-group rg-demo --vnet-name spokeVnetBlue --name spokeBlueSubnet1 --address-prefix 100.102.0.0/24
+        az network vnet subnet create --resource-group rg-demo --vnet-name spokeVnetBlue --name spokeBlueSubnet2 --address-prefix 100.102.1.0/24
+        az network vnet subnet create --resource-group rg-demo --vnet-name spokeVnetBlue --name spokeBlueSubnet3 --address-prefix 100.102.2.0/24
+        az network vnet subnet create --resource-group rg-demo --vnet-name spokeVnetBlue --name spokeBlueSubnet4 --address-prefix 100.102.3.0/24
+        
+        #Step 4: Create vnet peering to create star(hub/spoke) topology
+        #
+        az network vnet peering create \
+          --name hubToSpokeRed \
+          --resource-group rg-demo \
+          --vnet-name hubVnet \
+          --remote-vnet spokeVnetRed \
+          --allow-vnet-access \
+          --allow-forwarded-traffic true
+        #
+        az network vnet peering create \
+          --name SpokeRedToHub \
+          --resource-group rg-demo \
+          --vnet-name spokeVnetRed \
+          --remote-vnet hubVnet \
+          --allow-vnet-access \
+          --allow-forwarded-traffic true
+        az network vnet peering create \
+          --name hubToSpokeBlue \
+          --resource-group rg-demo \
+          --vnet-name hubVnet \
+          --remote-vnet spokeVnetBlue \
+          --allow-vnet-access \
+          --allow-forwarded-traffic true
+        #
+        az network vnet peering create \
+          --name SpokeBlueToHub \
+          --resource-group rg-demo \
+          --vnet-name spokeVnetBlue \
+          --remote-vnet hubVnet \
+          --allow-vnet-access \
+          --allow-forwarded-traffic true
+```
---
+++ verify.bash
@@ -0,0 +1,2 @@
+az network vnet list -o table
+az network vnet peering list --resource-group rg-demo --vnet-name hubVnet -o table
